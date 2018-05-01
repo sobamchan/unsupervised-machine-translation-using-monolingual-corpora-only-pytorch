29d6e7a9e615e349e250389ddea8ee6a94f4f7da
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2018-05-01 22:10:44 +0900

    word by word translation script

--- libs/word_translation.py
+++ libs/word_translation.py
@@ -1,4 +1,7 @@
+import argparse
 import numpy as np
+from tqdm import tqdm
+from libs.vocabulary import Vocabulary
 
 
 class WordTranslator:
@@ -26,3 +29,55 @@ class WordTranslator:
 
         selected_word_idxes = np.argmax(scores, axis=1)
         return self.tgt_vocab.decode(selected_word_idxes)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input-file',
+                        type=str,
+                        required=True)
+    parser.add_argument('--output-file',
+                        type=str,
+                        required=True)
+    parser.add_argument('--src-vec-file',
+                        type=str,
+                        default='input/vectors-en.npy',
+                        help='npy file')
+    parser.add_argument('--tgt-vec-file',
+                        type=str,
+                        default='input/vectors-fr.npy',
+                        help='npy file')
+    parser.add_argument('--src-vocab-file',
+                        type=str,
+                        default='input/vectors-en-vocab.txt',
+                        help='txt file')
+    parser.add_argument('--tgt-vocab-file',
+                        type=str,
+                        default='input/vectors-fr-vocab.txt',
+                        help='txt file')
+    parser.add_argument('--src-vocab-size',
+                        type=int,
+                        default=int(1e+10))
+    parser.add_argument('--tgt-vocab-size',
+                        type=int,
+                        default=int(1e+10))
+    args = parser.parse_args()
+
+    envec = np.load(args.src_vec_file)
+    frvec = np.load(args.tgt_vec_file)
+    with open(args.src_vocab_file) as f:
+        enwords = [line.strip() for line in f.readlines()]
+    with open(args.tgt_vocab_file) as f:
+        frwords = [line.strip() for line in f.readlines()]
+    envocab = Vocabulary(args.src_vocab_size)
+    envocab.build_vocab_from_words(enwords)
+    frvocab = Vocabulary(args.tgt_vocab_size)
+    frvocab.build_vocab_from_words(frwords)
+
+    WT = WordTranslator(envec, envocab, frvec, frvocab)
+    input_lines = open(args.input_file).readlines()
+    output_f = open(args.output_file, 'a')
+    for input_line in tqdm(input_lines):
+        output_line = WT.translate(input_line)
+        output_f.write(output_line)
+        output_f.write('\n')
